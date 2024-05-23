bdee034532f9dd9c3823afc6d676830ee600c860
ykhadilkar <kyatin1@gmail.com>
2024-05-23 16:10:12 -0400

    public subnet tags

---
+++ lb-controller.tf
@@ -0,0 +1,79 @@
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+# resource "helm_release" "aws_load_balancer_controller" {
+#   name       = "aws-load-balancer-controller"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   namespace  = "kube-system"
+
+#   set {
+#     name  = "clusterName"
+#     value = module.eks.cluster_name
+#   }
+
+#   set {
+#     name  = "region"
+#     value = var.region
+#   }
+
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "false"
+#   }
+
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller"
+#   }
+
+#   depends_on = [module.eks]
+# }
+
+# resource "kubernetes_service_account" "aws_load_balancer_controller" {
+#   metadata {
+#     name      = "aws-load-balancer-controller"
+#     namespace = "kube-system"
+#     labels = {
+#       "app.kubernetes.io/name" = "aws-load-balancer-controller"
+#     }
+#     annotations = {
+#     "eks.amazonaws.com/role-arn" = aws_iam_role.eks_lb_controller_role.arn
+#     }
+#   }
+
+#   automount_service_account_token = true
+# }
+
+# resource "aws_iam_role" "eks_lb_controller_role" {
+#   name = "${local.cluster_name}-lb-controller-role"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "eks.amazonaws.com"
+#       }
+#       Action = "sts:AssumeRole"
+#     }]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "eks_lb_controller_policy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+#   role       = aws_iam_role.eks_lb_controller_role.name
+# }
--- main.tf
+++ main.tf
@@ -33,16 +33,16 @@ module "vpc" {
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.2.0/24"]
+  public_subnets  = ["10.0.2.0/24", "10.0.5.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
-  # public_subnet_tags = {
-  #   "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  #   "kubernetes.io/role/elb"                      = 1
-  # }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
